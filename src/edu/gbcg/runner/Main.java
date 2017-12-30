7c25d63cc6ccf70f2a67588499a684b31e9147cc
Sean Grimes <spg63@cs.drexel.edu>
2017-12-29 20:32:35 -0500

    Pre index

--- src/edu/gbcg/runner/Main.java
+++ src/edu/gbcg/runner/Main.java
@@ -8,6 +8,7 @@ import edu.gbcg.utils.c;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception{
