f6d7217cae43c92b5fd27d52cc25bdb173ee409e
Sean Grimes <spg63@cs.drexel.edu>
2017-12-31 16:24:41 -0500

    comments should work

--- src/edu/gbcg/dbcreator/Reddit/CommentsJsonToDBWorker.java
+++ src/edu/gbcg/dbcreator/Reddit/CommentsJsonToDBWorker.java
@@ -2,6 +2,7 @@ package edu.gbcg.dbcreator.Reddit;
 
 import edu.gbcg.dbcreator.DBCommon;
 import edu.gbcg.utils.TSL;
+import edu.gbcg.utils.TimeFormatter;
 import org.json.JSONObject;
 
 import java.sql.Connection;
@@ -76,7 +77,93 @@ public class CommentsJsonToDBWorker implements Runnable{
 
             for(int i = 0; i < numObjects_; ++i){
                 int key = 1;
+                // column = author
+                String author = this.jsonObjects_.get(i).optString("author", "null");
+                ps.setString(key, author); ++key;
 
+                // column = author_flair_text
+                String author_flair_text = this.jsonObjects_.get(i).optString("author_flair_text", "null");
+                ps.setString(key, author_flair_text); ++key;
+
+                // column = body
+                String body = this.jsonObjects_.get(i).optString("body", "null");
+                ps.setString(key, body); ++key;
+
+                // column = can_gild
+                int can_gild = this.jsonObjects_.get(i).optBoolean("can_gild", false) ? 1 : 0;
+                ps.setInt(key, can_gild); ++key;
+
+                // column = controversial_score
+                int cont = this.jsonObjects_.get(i).optInt("controversiality", 0);
+                ps.setInt(key, cont); ++key;
+
+                // column = created_dt
+                Long created_utc = this.jsonObjects_.get(i).optLong("created_utc", 0);
+                String created_dt = TimeFormatter.javaDateTimeToSQLDateTime(
+                        TimeFormatter.utcToLDT(created_utc.toString())
+                );
+                ps.setString(key, created_dt); ++key;
+
+                // column = distinguished
+                String dist = this.jsonObjects_.get(i).optString("distinguished", "null");
+                ps.setString(key, dist); ++key;
+
+                // column = been_edited
+                int edited = this.jsonObjects_.get(i).optBoolean("edited", false) ? 1 : 0;
+                ps.setInt(key, edited); ++key;
+
+                // column = gilded
+                int gilded = this.jsonObjects_.get(i).optInt("gilded", 0);
+                ps.setInt(key, gilded); ++key;
+
+                // column = pid
+                String pid = this.jsonObjects_.get(i).optString("id", "null");
+                ps.setString(key, pid); ++key;
+
+                // column = is_submitter
+                int submitter = this.jsonObjects_.get(i).optBoolean("is_submitter", false) ? 1 : 0;
+                ps.setInt(key, submitter); ++key;
+
+                // column = link_id
+                String link_id = this.jsonObjects_.get(i).optString("link_id", "null");
+                ps.setString(key, link_id); ++key;
+
+                // column = parent_id
+                String parent_id = this.jsonObjects_.get(i).optString("parent_id", "null");
+                ps.setString(key, parent_id); ++key;
+
+                // column = permalink
+                String permalink = this.jsonObjects_.get(i).optString("permalink", "null");
+                ps.setString(key, permalink); ++key;
+
+                // column = scraped_on
+                Long retrieved_on = this.jsonObjects_.get(i).optLong("retrieved_on", 0);
+                String scraped_on = TimeFormatter.javaDateTimeToSQLDateTime(
+                        TimeFormatter.utcToLDT(retrieved_on.toString())
+                );
+                ps.setString(key, scraped_on); ++key;
+
+                // column = score
+                int score = this.jsonObjects_.get(i).optInt("score", 0);
+                ps.setInt(key, score); ++key;
+
+                // column = is_stickied
+                int sticked = this.jsonObjects_.get(i).optBoolean("stickied", false) ? 1 : 0;
+                ps.setInt(key, sticked); ++key;
+
+                // column = subreddit_name
+                String sub_name = this.jsonObjects_.get(i).optString("subreddit", "null");
+                ps.setString(key, sub_name); ++key;
+
+                // column = subreddit_id
+                String sub_id = this.jsonObjects_.get(i).optString("subreddit_id", "null");
+                ps.setString(key, sub_id); ++key;
+
+                // column = subreddit_type
+                String sub_type = this.jsonObjects_.get(i).optString("subreddit_type", "null");
+                ps.setString(key, sub_type);
+
+                ps.addBatch();
             }
 
             ps.executeBatch();
