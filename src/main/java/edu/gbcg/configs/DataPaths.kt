813c2406e10cdee8ac56596908d4612c7c066c55
Sean Grimes <spg63@cs.drexel.edu>
2018-01-09 13:01:12 -0500

    finals in kotlin

---
+++ src/main/java/edu/gbcg/configs/DataPaths.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 Sean Grimes. All Rights Reserved.
+ * License: MIT
+ */
+
+package edu.gbcg.configs
+
+/**
+ * The different paths to the DB, Json, and CSV data depending on which machine is running the
+ * code and whether or not testing_mode has been enabled
+ */
+object DataPaths{
+    // File paths when running in 'TESTING_MODE' (i.e. on my MBP with limited data are LOCAL)
+    const val LOCAL_SUB_DATA_PATH   = "LocalData/raw/submission/"
+    const val LOCAL_COM_DATA_PATH   = "LocalData/raw/comments/"
+
+    // File paths when running on the research machine
+    const val SUB_DATA_PATH         = "A:/Data/Uncompressed/Reddit/Submissions/"
+    const val COM_DATA_PATH         = "A:/Data/Uncompressed/Reddit/Comments/"
+
+    // Path to TESTING_MODE db files
+    const val LOCAL_SUB_DB_PATH     = "LocalDB/RedditSubs/"
+    const val LOCAL_COM_DB_PATH     = "LocalDB/RedditComs/"
+
+    const val SUB_DB_PREFIX         = "RS_DB"
+    const val COM_DB_PREFIX         = "RC_DB"
+    const val DB_POSTFIX            = Finals.DB_TYPE_EXT
+}
