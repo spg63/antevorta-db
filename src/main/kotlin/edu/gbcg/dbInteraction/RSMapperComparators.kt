5c3f6c0b9a40001861275b5e8f967c078855460c
Sean Grimes <spg63@cs.drexel.edu>
2018-04-26 18:52:50 -0400

    god damn comparator is complicated for DB results

--- src/main/kotlin/edu/gbcg/dbInteraction/RSMapperComparators.kt
+++ src/main/kotlin/edu/gbcg/dbInteraction/RSMapperComparators.kt
@@ -12,7 +12,6 @@ import edu.gbcg.utils.TSL
 object RSMapperComparators: Comparator<RSMapper> {
 
     var columnsWithOrder: OrderBySelection? = null
-    var primaryColumnBadName = false
 
     override fun compare(rs1: RSMapper, rs2: RSMapper): Int {
         // columns with order *must* be set before this function can work, it's the only way to know how to sort
@@ -26,19 +25,21 @@ object RSMapperComparators: Comparator<RSMapper> {
 
         // Return quickly if the column name supplied does not match with an existing column name in the RSMapper.
         // This returns 0, indicating equal values, and no sorting will happen
-        if(primaryColumnBadName || badColumnName(rs1, columnsWithOrder!!.primaryColumn)){
-            
-        }
+        if(badColumnName(rs1, columnsWithOrder!!.primaryColumn)) return 0
 
         // Now determine how to do the sort based on the number of columns and the ordering
-        if(columnsWithOrder!!.numColumnsToSortBy == 1){
+        if(columnsWithOrder!!.numColumnsToSortBy == 1 && columnsWithOrder!!.primaryIsAscending){
             return compareAscending(rs1, rs2, columnsWithOrder!!.primaryColumn)
         }
+        else if(columnsWithOrder!!.numColumnsToSortBy == 1 && !columnsWithOrder!!.primaryIsAscending){
+            return compareDescending(rs1, rs2, columnsWithOrder!!.primaryColumn)
+        }
+
+        // I guess we somehow missed all the possible options, return 0 for no sort
         return 0
     }
 
-    private fun compareAscending(rs1: RSMapper, rs2: RSMapper, columnName: String): Int {
-        if(badColumnName(rs1, columnName)) return 0
+    private fun compareAscending(rs1: RSMapper, rs2: RSMapper, columnName: String, numCols: Int = 1): Int {
         return when{
             rs1.getString(columnName) > rs2.getString(columnName) -> 1
             rs1.getString(columnName) == rs2.getString(columnName) -> 0
@@ -46,11 +47,21 @@ object RSMapperComparators: Comparator<RSMapper> {
         }
     }
 
+    private fun compareDescending(rs1: RSMapper, rs2: RSMapper, columnName: String, numCols: Int = 1): Int {
+        return when {
+            rs1.getString(columnName) > rs2.getString(columnName) -> -1
+            rs1.getString(columnName) == rs2.getString(columnName) -> 0
+            else -> 1
+        }
+    }
+
+
     /*
         If the column doesn't exist, or it's not comparable, getString will return "". In this case return true, and
         the calling function will know to return a no-compare value, 0, and move on
      */
     private fun badColumnName(rs: RSMapper, columnName: String): Boolean {
+        TSL.get().err("RSMapperComparators.badColumnName was true, sorting was not completed!")
         return rs.getString(columnName) == ""
     }
 }
