feb34bd95e297e648e5eb30a7b7636e000f7dd34
Sean Grimes <spg63@cs.drexel.edu>
2019-12-23 23:59:11 -0500

    Updated telemetry keys and added telemetry string getter

--- src/main/kotlin/edu/antevortadb/configs/Telemetry.kt
+++ src/main/kotlin/edu/antevortadb/configs/Telemetry.kt
@@ -1,35 +1,40 @@
 package edu.antevortadb.configs
 
+import javalibs.NetworkUtils
 import javalibs.SysHelper
-import javalibs.TSL
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.DataOutputStream
-import java.io.IOException
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.net.InetSocketAddress
-import java.net.Socket
-import java.net.URL
-import javax.swing.text.html.parser.TagElement
-import kotlin.math.log
 
 class Telemetry {
+    val sysHelper: SysHelper
+    val networkUtils: NetworkUtils
+    val ipAddr: String
+
+
+    init {
+        this.sysHelper = SysHelper.get()
+        this.networkUtils = NetworkUtils.get()
+        this.ipAddr = this.sysHelper.externalIPAddr()
+    }
+
     companion object {
         /* ---------- Telemetry Keys ---------------------------------------------------*/
+        // OS Keys
         const val OS_FULL   = "os.fullInformation"
         const val OS_BUILD  = "os.buildNumber"
         const val OS_CODE   = "os.codeName"
         const val OS_MANU   = "os.manfacturer"
         const val OS_NAME   = "os.name"
         const val OS_VER    = "os.version"
+
+        // User Keys
         const val USER_NAME = "user.name"
         const val USER_HOME = "user.home"
-        const val WORKING   = "user.dir"
+        const val WORKING   = "user.currentWorkingDir"
         const val IP_ADDR   = "ip.addr"
         const val HOST_NAME = "user.hostName"
         const val JAVA_VER  = "java.version"
 
+        // Hardware Keys
         const val HD_MANU   = "hd.manufacturer"
         const val HD_MODEL  = "hd.hardwareModel"
         const val HD_SERI   = "hd.hardwareSerial"
@@ -42,9 +47,9 @@ class Telemetry {
         const val HT_ENABLE = "hd.hasHyperThreading"
         const val CPU_BASE  = "hd.cpuBaseFreq"
         const val CPU_TMP   = "hd.cpuTemp"
-        const val MAX_MEM   = "hd.memoryMax"
-        const val AVAIL_MEM = "hd.memoryAvailable"
-        const val FREE_MEM  = "hd.memoryFree"
+        const val MAX_MEM   = "hd.JVM_memoryMax"
+        const val AVAIL_MEM = "hd.JVM_memoryAvailable"
+        const val FREE_MEM  = "hd.JVM_memoryFree"
         const val MAX_HDD   = "hd.storageTotal"
         const val FREE_HDD  = "hd.storageFree"
 
@@ -55,16 +60,34 @@ class Telemetry {
         const val timeout   = 2000
     }
 
-    val sysHelper = SysHelper.get()
-
-    fun push(){
+    /**
+     * Gather the telemetry data and push it to ripper for collection
+     */
+    fun push() {
         // Gather the IP address. Want to get this first, if there's a timeout this
         // function just skips gathering telemetry since I don't want to try another
         // network connection at this point and waste more time. Also check for
         // ripper's status and skip if ripper isn't reachable
-        val ipAddr = this.sysHelper.externalIPAddr()
-        if(this.sysHelper.BAD_NETWORK_ATEMPT == ipAddr || !isRipperAvailable())
+        if(this.sysHelper.BAD_NETWORK_ATEMPT == this.ipAddr || !isRipperAvailable())
             return
+
+        val json = buildTelemetryObject()
+
+        // Push the json object to ripper, no response necessary
+        networkUtils.writeWithoutResponse(host, port, json.toString())
+    }
+
+    /**
+     * Get all of the telemetry data for the system this code runs on
+     * @return The telemetry JSON object as a String
+     */
+    fun getTelemetryString(): String {
+        val jsonPrintSpacing = 4
+        return buildTelemetryObject().toString(jsonPrintSpacing)
+    }
+
+    // Build the full JSON object that contains the telemetry data
+    private fun buildTelemetryObject(): JSONObject {
 
         val json = JSONObject()
         json.put("HOSTNAME", host)
@@ -84,7 +107,7 @@ class Telemetry {
         json.put(Telemetry.USER_NAME, this.sysHelper.userName())
         json.put(Telemetry.USER_HOME, this.sysHelper.userHome())
         json.put(Telemetry.WORKING, this.sysHelper.userWorking())
-        json.put(Telemetry.IP_ADDR, ipAddr)
+        json.put(Telemetry.IP_ADDR, this.ipAddr)
         json.put(Telemetry.HOST_NAME, this.sysHelper.hostName())
         json.put(Telemetry.JAVA_VER, this.sysHelper.javaVer())
 
@@ -97,9 +120,9 @@ class Telemetry {
         // Double.MAX_VALUE can be ambigious on the receiving end
         val batTimeRemaining = this.sysHelper.batteryTimeRemainingSeconds()
         val batTime =   if(batTimeRemaining.compareTo(Double.MAX_VALUE) == 0)
-                            "Unlimited -- Running on AC (probably)"
-                        else
-                            batTimeRemaining
+            "Unlimited -- Running on AC (probably)"
+        else
+            batTimeRemaining.toString()
         json.put(Telemetry.BAT_REM, batTime)
         json.put(Telemetry.BAT_CYCL, this.sysHelper.batteryCycleCount())
         json.put(Telemetry.PHYS_CORE, this.sysHelper.physicalCPUCoreCount())
@@ -113,32 +136,11 @@ class Telemetry {
         json.put(Telemetry.MAX_HDD, this.sysHelper.rootTotalSpace())
         json.put(Telemetry.FREE_HDD, this.sysHelper.rootFreeSpace())
 
-        try {
-            val sock = Socket(host, port)
-            val serverWriter = DataOutputStream(sock.getOutputStream())
-            //TSL.get().die(json.toString())
-            serverWriter.writeBytes(json.toString() + "\n")
-            serverWriter.flush()
-            serverWriter.close()
-            sock.close()
-        }
-        catch(e: Exception){
-            // Log it and move on, this is far from necessary to run the program
-            TSL.get().exception(e)
-        }
+        return json
     }
 
     // Checks if a host is available on a specific port. If not return false
-    fun isRipperAvailable(): Boolean {
-        return try{
-            //val sock = Socket()
-            //sock.connect(InetSocketAddress(host, port), timeout)
-            TSL.get().err("New way to check for ripper availability")
-            true
-        }
-        catch(isDown: IOException){
-            false
-        }
-    }
+    private fun isRipperAvailable(): Boolean = networkUtils.pingHost(host, port, timeout)
+
 }
 
